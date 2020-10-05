a070eaae2a8d4da95a081c864506e48095488c4d
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2020-10-05 12:14:04 +0800

    Setup Django project with initial (failing) functional test

---
+++ functional_tests/test_landing_page.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+
+class LandingPageTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_landing_page_loads_properly(self):
+        # John heard of this new Django template called "Aeternae" that he could use with his next project.
+        # He visited its website and saw from the title and site header that this site is, in fact, of the Aeternae template.
+        self.selenium.get(self.live_server_url)
+        page_title = self.selenium.find_element_by_tag_name("title").text
+        self.assertContains(page_title, "Aeternae")
+        header_title = self.selenium.find_element_by_tag_name("h1").text
+        self.assertContains(header_title, "Aeternae")
+
+        # Intrigued by the "Find out More" call-to-action, he clicked it and the page scrolled to the "About" section
+        cta_button = self.selenium.find_element_by_link_text("Find out more")
+        cta_button.click()
+        # Finding text by viewport isn't available in the Selenium API yet, so let's just check the the current url is still the landing page
+        self.assertEquals(self.live_server_url, self.selenium.current_url)
+
+        # Scrolling through the page, he reached the bottom and found a copyright notice.
+        # Again, viewport testing isn't available so let's just assert the the footer text is in the page's HTML
+        footer_text = self.find_element_by_tag_name("footer").text
+        current_year = datetime.today().year
+        self.assertContains(
+            footer_text, f"Copyright {current_year} - hoplite.tech")
+
+        # Satisfied, he went to bed.
