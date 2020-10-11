b75a6d0028d4cb1cb682281dcfcc40ede05f6980
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2020-10-12 04:29:38 +0800

    Add STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Custom User model
 AUTH_USER_MODEL = 'accounts.Account'
