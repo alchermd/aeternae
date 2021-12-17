6f81d580d4cb50a973ded7a3b8d30e79ead8d203
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-17 13:51:01 +0800

    Delete pages directory

--- pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from pages import views
-
-app_name = "pages"
-urlpatterns = [
-    path("", views.home, name="home")
-]
--- pages/views.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render
-from django.urls import reverse_lazy
-
-
-@user_passes_test(lambda u: u.is_anonymous, login_url=reverse_lazy("dashboard:home"), redirect_field_name=None)
-def home(request):
-    return render(request, "pages/index.html")
