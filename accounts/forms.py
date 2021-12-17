8e3574dbdad7b5e39c6b059b65ce550d6638e885
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-17 13:37:00 +0800

    Delete accounts directory

--- accounts/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from accounts.models import Account
-
-
-class RegistrationForm(UserCreationForm):
-    class Meta:
-        model = Account
-        fields = ('email', 'first_name', 'last_name', 'password1', 'password2')
